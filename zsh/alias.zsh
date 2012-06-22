8c079cc5dd5ce574e60e50b4cecee6bf7adda7a0
Nishibayashi Takuji <takuji@senchan.jp>
2012-06-22 15:11:21 +0900

    aliasの設定をtakuji_dotfilesから引用して少し追加

---
+++ zsh/alias.zsh
@@ -0,0 +1,28 @@
+case "${OSTYPE}" in
+freebsd*|darwin*)
+    alias ls="ls -G -w"
+    ;;
+linux*)
+    alias ls="ls --color"
+    ;;
+esac
+
+alias la="ls -la"
+alias lf="ls -F"
+alias ll="ls -l"
+alias lh="ls -h"
+
+alias du="du -h"
+alias df="df -h"
+
+alias su="su -l"
+
+alias s=screen
+alias tt="tmux -2 new-session"
+alias tr="tmux attach-session"
+alias tv="tmux new-window vim "
+
+alias vi="vim"
+alias q="exit"
+alias sudo="sudo "
+alias zmv='noglob zmv -W'
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -111,6 +111,7 @@ export CLICOLOR=true
 
 #外部ファイルの読み込み
 source $DOTFILES/zsh/prompt.zsh
+source $DOTFILES/zsh/alias.zsh
 
 #perlbrew
 [[ -s $HOME/perl5/perlbrew/etc/bashrc ]] && source $HOME/perl5/perlbrew/etc/bashrc
