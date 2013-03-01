7a8408d15ecc5fd82abcf13345bcc852b3646a68
Nishibayashi Takuji <takuji31@gmail.com>
2013-03-01 14:30:50 +0900

    add Dist::Maker

---
+++ dim/config.pl
@@ -0,0 +1,11 @@
+#!perl
+# This file is managed by /home/takuji/.plenv/versions/5.16.2/bin//dim.
+{
+  template => {
+    default => 'DefaultMB'
+  },
+  user => {
+    email => 'takuji31@gmail.com',
+    name => 'Nishibayashi Takuji'
+  }
+}
