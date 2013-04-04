f9c174d7cd2ef0516eb457f6226970b1c07f50a8
Nishibayashi Takuji <takuji31@gmail.com>
2013-04-04 18:54:20 +0900

    フォントの設定を追加

--- emacs/conf/cocoa-emacs-display.el
+++ emacs/conf/cocoa-emacs-display.el
@@ -43,3 +43,39 @@
 ;; 時計のフォーマット
 (setq display-time-string-forms '((format
   "%s/%s(%s) %s:%s" month day dayname 24-hours minutes)))
+
+;;フォント設定
+(when (and (>= emacs-major-version 24)
+           (eq window-system 'ns))
+  ;; フォントセットを作る
+  (let* ((fontset-name "myfonts") ; フォントセットの名前
+         (size 14) ; ASCIIフォントのサイズ [9/10/12/14/15/17/19/20/...]
+         (asciifont "Source Code Pro") ; ASCIIフォント
+         (jpfont "Ricty") ; 日本語フォント
+         (font (format "%s-%d:weight=normal:slant=normal" asciifont size))
+         (fontspec (font-spec :family asciifont))
+         (jp-fontspec (font-spec :family jpfont)) 
+         (fsn (create-fontset-from-ascii-font font nil fontset-name)))
+    (set-fontset-font fsn 'japanese-jisx0213.2004-1 jp-fontspec)
+    (set-fontset-font fsn 'japanese-jisx0213-2 jp-fontspec)
+    (set-fontset-font fsn 'katakana-jisx0201 jp-fontspec) ; 半角カナ
+    (set-fontset-font fsn '(#x0080 . #x024F) fontspec)    ; 分音符付きラテン
+    (set-fontset-font fsn '(#x0370 . #x03FF) fontspec)    ; ギリシャ文字
+    )
+ 
+  ;; デフォルトのフレームパラメータでフォントセットを指定
+  (add-to-list 'default-frame-alist '(font . "fontset-myfonts"))
+ 
+  ;; フォントサイズの比を設定
+  (dolist (elt '(("^-apple-hiragino.*"               . 1.2)
+		 (".*osaka-bold.*"                   . 1.2)
+		 (".*osaka-medium.*"                 . 1.2)
+		 (".*courier-bold-.*-mac-roman"      . 1.0)
+		 (".*monaco cy-bold-.*-mac-cyrillic" . 0.9)
+		 (".*monaco-bold-.*-mac-roman"       . 0.9)))
+    (add-to-list 'face-font-rescale-alist elt))
+ 
+  ;; デフォルトフェイスにフォントセットを設定
+  ;; # これは起動時に default-frame-alist に従ったフレームが
+  ;; # 作成されない現象への対処
+  (set-face-font 'default "fontset-myfonts"))
