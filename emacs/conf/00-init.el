47d945963284da1b6a73b0eec99d2b99f9b03081
Nishibayashi Takuji <takuji31@gmail.com>
2013-04-04 18:35:49 +0900

    emacswikiからリストを取得してるのがやたら遅い割にそんなに使わないので、無効にする

--- emacs/conf/00-init.el
+++ emacs/conf/00-init.el
@@ -5,7 +5,7 @@
 
 (require 'auto-install nil t)
 (setq auto-install-directory "~/.emacs.d/elisp")
-(auto-install-update-emacswiki-package-name t)
+;;(auto-install-update-emacswiki-package-name t)
 (auto-install-compatibility-setup)
 
 (require 'auto-complete-config)
