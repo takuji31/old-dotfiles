f290e7b7eaf423336068ff9c022432cd8616241a
Takuji Nishibayashi <takuji31@gmail.com>
2014-01-05 23:23:50 +0900

    Brewfileを追加

---
+++ brew/lanchctl.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#mongodb
+ln -sfv /usr/local/opt/mongodb/*.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mongodb.plist
+
+#mysql
+ln -sfv /usr/local/opt/mysql/*.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
+
+#nginx
+ln -sfv /usr/local/opt/nginx/*.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.nginx.plist
+
+#redis
+ln -sfv /usr/local/opt/redis/*.plist ~/Library/LaunchAgents
+launchctl load ~/Library/LaunchAgents/homebrew.mxcl.redis.plist
